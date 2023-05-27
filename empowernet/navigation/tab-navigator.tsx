e70cd3645cc3dd2d94dae0ffdafee0be5ff418d3
ashlynpark <ashlynpark6@gmail.com>
2023-05-26 23:34:45 -0700

    saved it all

--- empowernet/navigation/tab-navigator.tsx
+++ empowernet/navigation/tab-navigator.tsx
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 
 
 import Home from '../screens/Home';
-import CompanyPage from '../screens/CompanyPage';
+import CompanyPage from '../screens/CompanyProfile';
 
 
 const Tab = createBottomTabNavigator();
